8857adfea9a2348731e22ef93a249d0f8c4c1748
s3bc40 <s3bc40@gmail.com>
2025-03-25 11:07:27 +0100

    feat: solved generics, traits, lifetimes and quiz

--- 15_traits/traits5.rs
+++ 15_traits/traits5.rs
@@ -19,9 +19,17 @@ impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
 // TODO: Fix the compiler error by only changing the signature of this function.
-fn some_func(item: ???) -> bool {
+fn some_func(item: (impl SomeTrait + OtherTrait)) -> bool {
     item.some_function() && item.other_function()
 }
+
+// Trait bound syntax
+// fn some_func<T>(item: T) -> bool
+// where
+//     T: SomeTrait + OtherTrait,
+// {
+//     item.some_function() && item.other_function()
+// }
 
 fn main() {
     // You can optionally experiment here.
